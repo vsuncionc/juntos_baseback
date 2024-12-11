a679a67ffd366acf6bcadc526205a2dfe1a8ddba
vladimirsc <Enable two-factor authentication (2FA)>
2024-12-11 06:16:57 -0500

    alternativas

--- src/main/java/com/base/base/api/AlternativaController.java
+++ src/main/java/com/base/base/api/AlternativaController.java
@@ -1,7 +1,16 @@
 package com.base.base.api;
 
+import com.base.base.dto.request.ConsultaColegiosRequest;
+import com.base.base.model.ResponseAppBean;
+import com.base.base.util.Constante;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/alternativa")
 @RequiredArgsConstructor
 public class AlternativaController {
+
+    @PostMapping(value = "listar", produces = "application/json")
+    public ResponseEntity<?> listarColegiosUbigeo(@RequestBody ConsultaColegiosRequest consultaColegiosRequest,
+                                                  HttpServletRequest request, HttpServletResponse response) {
+        ResponseAppBean responseAppBean = new ResponseAppBean();
+        try {
+            responseAppBean.setStatus(Constante.RESPONSE_OK);
+            return new ResponseEntity<>(responseAppBean, HttpStatus.OK);
+        }catch (Exception e){
+            responseAppBean.setStatus(Constante.RESPONSE_ERROR);
+            responseAppBean.setMessage(Constante.MENSAJE_ERROR);
+            responseAppBean.setMessageBackend(e.getMessage());
+            return new ResponseEntity<>(responseAppBean, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+     }
 }
